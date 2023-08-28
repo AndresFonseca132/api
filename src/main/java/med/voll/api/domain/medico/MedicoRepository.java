bfd905fcb76434be308891b6182b9688431034fc
AndresFonseca01 <affonseca613@soy.sena.edu.co>
2023-08-28 14:20:57 -0500

    Arreglo de la consulta

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -12,11 +12,14 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
 
     @Query("""
             select m from Medico m
-            where m.activo= true and 
-            m.especialidad =: especialidad and
-            m.id not in(
-            select c.medico.id from Consulta c
-            c.data=:fecha
+            where m.activo= true 
+            and 
+            m.especialidad =: especialidad 
+            and
+            m.Id not in(
+                select c.medico.Id from Consulta c
+                where 
+                c.data=:fecha
             )
             order by rand()
             limit 1
